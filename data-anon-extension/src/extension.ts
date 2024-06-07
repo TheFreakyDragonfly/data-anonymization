e84e53e5c5382a0756561efe0aedf1c669ae8ca5
David_Marangoni <david.marangoni@web.de>
2024-06-07 11:29:48 +0200

    Added short loading message between screens

--- data-anon-extension/src/extension.ts
+++ data-anon-extension/src/extension.ts
@@ -39,13 +39,17 @@ export function activate(context: vscode.ExtensionContext) {
 				switch (message.command) {
 					case 'open_db_cs':
 						console.log('open db cs:  ' + message.text);
+
+						used_cs = message.text;
 						
-						connectAndQueryDB_plus_buildSelectionPage_cs(panel, message.text);
+						connectAndQueryDB_plus_buildSelectionPage(panel);
 						break;
 					case 'open_db_config':
 						console.log('open db config:  ' + message.text);
+
+						used_config = config_string_to_config(message.text);
 						
-						connectAndQueryDB_plus_buildSelectionPage_config(panel, message.text);
+						connectAndQueryDB_plus_buildSelectionPage(panel);
 						break;
 
 					case 'load_connection_form':
@@ -54,6 +58,8 @@ export function activate(context: vscode.ExtensionContext) {
 
 					case 'python':
 						write_order(message.text);
+						//call_python();
+						panel.webview.html = getDatabaseSelectionWebviewContent();
 						break;
 
 					case 'report':
@@ -82,21 +88,44 @@ function getDatabaseSelectionWebviewContent() {
 				<script>
 					const vscode = acquireVsCodeApi();
 
+					function message_default_cs() {
+						vscode.postMessage({
+							command: 'open_db_cs',
+							text: 'Server=tcp:sqls-dataanon-dev-001.database.windows.net,1433;Initial Catalog=Northwind;Persist Security Info=False;User ID=data-anon;Password=Lantanio13891!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
+						});
+						show_trying_to_open();
+					}
+
+					function message_default_config() {
+						vscode.postMessage({
+							command: 'open_db_config',
+							text: 'data-anon;Lantanio13891!;sqls-dataanon-dev-001.database.windows.net;Northwind'
+						});
+						show_trying_to_open();
+					}
+
 					function message_connectionstring() {
+						
 						vscode.postMessage({
 							command: 'open_db_cs',
 							text: document.getElementById("connectionString").value
 						});
+						show_trying_to_open();
 					}
 					function message_config() {
 						let user = document.getElementById("user").value;
 						let password = document.getElementById("password").value;
 						let server = document.getElementById("server").value;
 						let database = document.getElementById("database").value;
+						
 						vscode.postMessage({
 							command: 'open_db_config',
 							text: user + ";" + password + ";" + server + ";" + database
 						});
+						show_trying_to_open();
+					}
+					function show_trying_to_open() {
+						document.body.innerHTML = "Loading ...";
 					}
 				</script>
 			</head>
@@ -105,11 +134,11 @@ function getDatabaseSelectionWebviewContent() {
 
 				<hr>
 
-				Connection String<br>
+				<p onclick="message_default_cs()">Connection String</p>
 				<input type="text" id="connectionString"/> <button onclick="message_connectionstring()">Open</button>
 
 				<hr>
-				SQL-Connection<br>
+				<p onclick="message_default_config()">SQL-Connection</p>
 				User <input id="user"/><br>
 				Password <input id="password"/><br>
 				Server <input id="server"/><br>
@@ -127,14 +156,8 @@ function getDatabaseSelectionWebviewContent() {
 	`;
 }
 
-// TODO SWITCH FOR USING USED_CS and so on + DOCUMENTATION COMMENTS
-function connectAndQueryDB_plus_buildSelectionPage_cs(panel: any, given_cs: string) {
-	used_cs = given_cs;
-	used_config = undefined;
-	connectAndQueryDB_plus_buildSelectionPage(panel, given_cs);
-}
-
-function connectAndQueryDB_plus_buildSelectionPage_config(panel: any, given_config: string) {
+/* Turns a string into a config */
+function config_string_to_config(given_config: string) {
 	let split = given_config.split(";", 4);
 	let config = {
 		user: split[0],
@@ -142,15 +165,24 @@ function connectAndQueryDB_plus_buildSelectionPage_config(panel: any, given_conf
 		server: split[2],
 		database: split[3],
 	};
-	used_cs = undefined;
-	used_config = config;
-	connectAndQueryDB_plus_buildSelectionPage(panel, config);
+	return config;
 }
 
-function connectAndQueryDB_plus_buildSelectionPage(panel: any, connection_object: any) {
+/* Builds Table-Selection HTML by connecting to server using global vars */
+function connectAndQueryDB_plus_buildSelectionPage(panel: any) {
 	let sql = require("mssql");
 
 	// Establish connection using either config or connectionstring
+	let connection_object;
+	if(used_cs !== undefined) {
+		connection_object = used_cs;
+	}
+	else if(used_config !== undefined) {
+		connection_object = used_config;
+	}
+	else {
+		connection_object = undefined;
+	}
 	sql.connect(connection_object, function (err: any) {
 		if (err) {
 			panel.webview.html += "err: " + err + "<br>";
@@ -252,6 +284,7 @@ function write_order(tables: string) {
 
 	let path_string = path.join(__dirname, '..', '..', 'order');
 
+	// Build content of order
 	let content = "[Anonymization Order]\n";
 	
 	let connection_section;
@@ -274,6 +307,7 @@ function write_order(tables: string) {
 	content += "[TABLES]\n";
 	content += tables;
 
+	// Write order to file
 	fs.writeFile(path_string, content, function(err : any) {
 		if(err) {
 			return console.log(err);
