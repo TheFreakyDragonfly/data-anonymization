f72c663f576e2e71c992dcd9b4571bcc136be732
Michael Du <michael.du@outlook.de>
2024-06-12 19:55:17 +0200

    added regex to trigger implementation

--- src/tmp_app.py
+++ src/tmp_app.py
@@ -1,4 +1,5 @@
 from finance import Finance
+from personal import Personal
 from re import match
 
 
@@ -10,11 +11,14 @@ def anonymizing_bank_data(data):
     elif match('[A-Z][a-zäöüß]+,\\s?[A-Z][a-zäöüß]+', str(data)):
         anonymized = Finance.anonymize_account_owner(False)
     elif match('[A-Z0-9]+', str(data)):
-        anonymized = Finance.anonymize_transaction_number(data)
+        anonymized = Finance.anonymize_by_replacing(data)
     elif match('', str(data)):  # HOW TO IDENTIFY COMPANIES
         anonymized = Finance.anonymize_transaction_recipient(data)
-    else:
-        print("Föck")
+
+
+def anonymizing_personal(data):
+    if match('\\d{2}.\\d{2}.\\d{4}', str(data)) or match('\\d{4}.\\d{2}.\\d{2}', str(data)):
+        anonymized = Personal.anonymizing_date(data)
 
 
 if __name__ == '__main__':
@@ -28,3 +32,11 @@ if __name__ == '__main__':
 
     anonymizing_bank_data("Hans Werner")
     anonymizing_bank_data("Jauch, Günther")
+
+    anonymizing_personal("10.03.2001")
+    anonymizing_personal("11-04-2000")
+    anonymizing_personal("06-12-2002")
+    anonymizing_personal("2003-05-13")
+    anonymizing_personal("2004/07/14")
+    anonymizing_personal("15/08/2005")
+    anonymizing_personal("09/16/2006")
