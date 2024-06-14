7e706ab45d7f9400c7d99696d218fba9b2cd1370
Michael Du <michael.du@outlook.de>
2024-06-14 12:46:06 +0200

    corrected statements cuz of restructuring

--- src/tmp_app.py
+++ src/tmp_app.py
@@ -11,19 +11,19 @@ def anonymizing_bank_data(data):
         anonymized = Finance.anonymize_iban(data)
     elif match(".*,.*", str(data).rstrip()):
         anonymized = Finance.anonymize_name(False, data.rstrip())
-    elif match('([A-Z][a-zäöüß\\-\\s]+\\s?)+', str(data).rstrip()):
-        anonymized = Finance.anonymize_name(True, data.rstrip())
     elif match('[A-Z0-9]+', str(data)):
         anonymized = Finance.anonymize_by_replacing(data)
-    elif match('', str(data)):  # HOW TO IDENTIFY COMPANIES
-        #anonymized = Finance.anonymize_transaction_recipient(data)
-        raise NotImplementedError("No Regex provided")
+    elif match('.*GmbH.*', str(data)):  # HOW TO IDENTIFY COMPANIES
+        anonymized = Finance.anonymize_company(data)
+        raise NotImplementedError("Provide a valid regex...")
 
 
 def anonymizing_personal(data):
     all_countries = [country.name for country in countries]
     if match('\\d{2}.\\d{2}.\\d{4}', str(data)) or match('\\d{4}.\\d{2}.\\d{2}', str(data)):
         anonymized = Personal.anonymizing_date(data)
+    elif match('([A-Z][a-zäöüß\\-\\s]+\\s?)+', str(data).rstrip()):
+        anonymized = Personal.anonymize_name(True, data.rstrip())
     elif str(Translator(to_lang="en").translate(data)) in all_countries:
         anonymized = Personal.anonymize_country(data)
     elif len(GeoText(str(data)).cities) > 0:
@@ -41,12 +41,12 @@ if __name__ == '__main__':
     anonymizing_bank_data(35123)
     anonymizing_bank_data('D6483')
 
-    anonymizing_bank_data("Hans Werner Baum")
-    anonymizing_bank_data("Günther-Jauch-Der-Top-G")
-    anonymizing_bank_data("Hans-Werner Busch")
-    anonymizing_bank_data("Hans-Werner Busch ")
-    anonymizing_bank_data("Jauch-Günther Hans, Dertop G")
-    anonymizing_bank_data("Hans Werner-Busch, Bill Cliton-Der-Juan")
+    anonymizing_personal("Hans Werner Baum")
+    anonymizing_personal("Günther-Jauch-Der-Top-G")
+    anonymizing_personal("Hans-Werner Busch")
+    anonymizing_personal("Hans-Werner Busch ")
+    anonymizing_personal("Jauch-Günther Hans, Dertop G")
+    anonymizing_personal("Hans Werner-Busch, Bill Cliton-Der-Juan")
 
     anonymizing_personal("10.03.2001")
     anonymizing_personal("11-04-2000")
@@ -59,3 +59,5 @@ if __name__ == '__main__':
     anonymizing_personal("Afghanistan")
     anonymizing_personal("Berlin")
     anonymizing_personal("Berlina")
+
+    anonymizing_bank_data("Diesel GmbH")
