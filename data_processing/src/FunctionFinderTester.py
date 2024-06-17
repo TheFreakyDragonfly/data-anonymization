e572ccc7d1514c21fe4b186272ad5b6b374b2f90
David_Marangoni <david.marangoni@web.de>
2024-06-17 14:44:13 +0200

    Added some more tests and tweaked function_finder

--- data_processing/src/FunctionFinderTester.py
+++ data_processing/src/FunctionFinderTester.py
@@ -1,11 +1,62 @@
 import unittest
 from function_finder import FunctionFinder
+from src.finance import Finance
 from standalone_anonymization_functions import *
+
+
+def matcher(cname, cval):
+    return FunctionFinder.match_function_by_regex_name_and_content(cname, cval, False)
 
 
 class TestFunctionFinder(unittest.TestCase):
     def test_finding_id(self):
-        self.assertEqual(anonymize_id, FunctionFinder.match_function_by_regex_name_and_content('id', 'AFAIK'))
+        self.assertEqual(
+            anonymize_id,
+            matcher('id', 'AFAIK')
+        )
+        self.assertNotEqual(
+            anonymize_id,
+            matcher('kid', 'joseph')
+        )
 
     def test_finding_company(self):
-        self.assertEqual(anonymize_id, FunctionFinder.match_function_by_regex_name_and_content('company', 'Google'))
+        self.assertEqual(
+            anonymize_company_name,
+            matcher('company', 'Google')
+        )
+
+    # generic case:
+    # def test_finding_x(self):
+    #     self.assertEqual()
+
+    def test_finding_email(self):
+        self.assertEqual(
+            anonymize_email,
+            matcher('email', 'email@email.com')
+        )
+        self.assertEqual(
+            anonymize_email,
+            matcher('xxxxx', 'email@email.com')
+        )
+
+    def test_finding_iban(self):
+        self.assertEqual(
+            Finance.anonymize_iban.__name__,
+            matcher('iban', '12').__name__
+        )
+        self.assertEqual(
+            Finance.anonymize_iban.__name__,
+            matcher('xxxx', 'NL69ABNA6863597098').__name__
+        )
+        self.assertEqual(
+            Finance.anonymize_iban.__name__,
+            matcher('xxxx', 'SA7771894124119856364796').__name__
+        )
+        self.assertNotEqual(
+            Finance.anonymize_iban.__name__,
+            matcher('taliban', '12').__name__
+        )
+        self.assertNotEqual(
+            Finance.anonymize_iban.__name__,
+            matcher('xxxx', 'DE12345').__name__
+        )
--- data_processing/src/function_finder.py
+++ data_processing/src/function_finder.py
@@ -12,11 +12,12 @@ all_countries = [country.name for country in countries]
 
 class FunctionFinder:
     @staticmethod
-    def match_function_by_regex_name_and_content(column_name, example_data):
+    def match_function_by_regex_name_and_content(column_name, example_data, allow_llm=True):
         """
         Tries to match a function to a column by examining name and content of column.
         :param column_name: Column name to use.
         :param example_data: Example piece of data.
+        :param allow_llm: Yes or no to asking llm for categorization.
         :return: Matched function.
         """
         example_data = str(example_data)
@@ -25,13 +26,14 @@ class FunctionFinder:
         c_low = column_name.lower()
 
         # match different cases
-        if 'id' in c_low and re.match('([a-zA-Z]+)|([0-9]+)', example_data):
+        if re.match(r"\bid\b", c_low) and re.match('([a-zA-Z]+)|([0-9]+)', example_data):
             return anonymize_id
         if 'company' in c_low:
             return anonymize_company_name
-        if 'email' in c_low:
+        if (re.match('email|e-mail', c_low)
+                or re.match(r"[^@]+@[^@]+\.[^@]+", example_data.lower())):
             return anonymize_email
-        if 'position' in c_low:
+        if 'position' in c_low or 'title' in c_low:
             return anonymize_position
         if 'name' in c_low:
             return anonymize_name
@@ -41,17 +43,17 @@ class FunctionFinder:
             return anonymize_phone
         if 'fax' in c_low:
             return anonymize_phone
-        if 'title' in c_low:
-            return anonymize_position
-        if 'iban' in c_low or re.match('[A-Z]{2}\\d{2}\\s?([A-Z0-9]+\\s?)+', str(example_data)):
+        if (re.match(r"\biban\b", c_low)
+                or re.match('[A-Z]{2}\\d{2}\\s?([A-Z0-9]+\\s?)+', str(example_data))
+                and len(example_data) >= 16):
             return Finance.anonymize_iban
-        if ('date' in c_low
+        if (re.match(r"\bdate\b", c_low)
                 or re.match('\\d{2}.\\d{2}.\\d{4}', str(example_data))
                 or re.match('\\d{4}.\\d{2}.\\d{2}', str(example_data))):
             return Personal.anonymizing_date
-        if 'country' in c_low or str(Translator(to_lang="en").translate(example_data)) in all_countries:
+        if re.match(r"\bcountry\b", c_low) or str(Translator(to_lang="en").translate(example_data)) in all_countries:
             return Personal.anonymize_country
-        if 'city' in c_low or len(GeoText(str(example_data)).cities) > 0:
+        if re.match(r"\bcity\b", c_low) or len(GeoText(str(example_data)).cities) > 0:
             return Personal.anonymize_city
         if re.match('([A-Z][a-zäöüß\\-\\s]+\\s?)+', str(example_data).rstrip()):
             return Personal.anonymize_name
@@ -61,12 +63,16 @@ class FunctionFinder:
             return Personal.anonymize_name
 
         # Ask llm as last measure
-        ext_print('Asking LLM about Column "' + column_name + '"')
-        llmi = LLMInteractor()
-        answer = llmi.ask_about_column_name(column_name)
-        ext_print('LLM Answer: ' + str(answer))
+        if allow_llm:
+            ext_print('Asking LLM about Column "' + column_name + '"')
+            llmi = LLMInteractor()
+            answer = llmi.ask_about_column_name(column_name)
+            ext_print('LLM Answer: ' + str(answer))
 
-        if answer:
-            return censor_fully
-        else:
-            return anonymize_nothing
+            if answer:
+                return censor_fully
+            else:
+                return anonymize_nothing
+
+        # if llm disallowed and no other match found
+        return censor_fully
